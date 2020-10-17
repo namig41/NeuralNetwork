65b69642d5b8ad0391f2c755bdf0ac2e16c679d4
namig41 <guseinovnamig41@gmail.com>
2020-10-17 04:41:54 +0300

    Refactoring code

--- main.m
+++ main.m
@@ -1,51 +1,51 @@
 clc;
-clear all;
+clear;
 close all;
-% 
-% tic
-% Class1 = NMatrix([randi(100, 500, 1) randi(50, 500, 1)]);
-% Class2 = NMatrix([randi(100, 500, 1) randi([51, 100], 500, 1)]);
-% 
-% Class1.unique();
-% Class2.unique();
-% 
-% clr = Classificator(2, 10);
-% 
-% C1 = NMatrix();
-% C2 = NMatrix();
-% 
-% [X,Y] = meshgrid(1:100, 1:100);
-% XY = Class1.unique_matrix([X(:) Y(:)]);
-% clear X; clear Y; clear ans;
-% 
-% for i=1:XY.n
-%     row = XY.get_row(i);
-%     if clr.binaryClassificationParzenWindow(Class1.matrix, Class2.matrix, row)
-%         C1.add_row(row);
-%     else
-%         C2.add_row(row);
-%     end
-% end
-% clear i; clear row;
-% 
-% Class1.add_col(ones([Class1.n 1]));
-% Class2.add_col(ones([Class2.n 1]) * -1);
+
+tic
+Class1 = NMatrix([randi(100, 500, 1) randi(50, 500, 1)]);
+Class2 = NMatrix([randi(100, 500, 1) randi([51, 100], 500, 1)]);
+
+Class1.unique();
+Class2.unique();
+
+clr = Classificator(2, 10);
+
+C1 = NMatrix();
+C2 = NMatrix();
+
+[X,Y] = meshgrid(1:100, 1:100);
+XY = Class1.unique_matrix([X(:) Y(:)]);
+clear X; clear Y; clear ans;
+
+for i=1:XY.n
+    row = XY.get_row(i);
+    if clr.binaryClassificationParzenWindow(Class1.matrix, Class2.matrix, row)
+        C1.add_row(row);
+    else
+        C2.add_row(row);
+    end
+end
+clear i; clear row;
+
+Class1.add_col(ones([Class1.n 1]));
+Class2.add_col(ones([Class2.n 1]) * -1);
 % [analysisError, h] = clr.bootstrap(NMatrix([Class1.matrix; Class2.matrix]), 50, 30);
-% El = clr.LOO(NMatrix([Class1.matrix; Class2.matrix]), 10);
-% 
-% figure(1)
-% hold on;
-% line([0 100], [51 51], 'color', 'm', 'linewidth', 2);
-% scatter(Class1.matrix(:, 1), Class1.matrix(:, 2), 'r', 'filled'); 
-% scatter(Class2.matrix(:, 1), Class2.matrix(:, 2), 'g', 'filled'); 
-% hold off;
-% 
-% figure(2)
-% hold on;
-% scatter(C1.matrix(:, 1), C1.matrix(:, 2), 'r', 'filled'); 
-% scatter(C2.matrix(:, 1), C2.matrix(:, 2), 'g', 'filled');
-% hold off;
-% 
+El = clr.LOO(NMatrix([Class1.matrix; Class2.matrix]), 10);
+
+figure(1)
+hold on;
+line([0 100], [51 51], 'color', 'm', 'linewidth', 2);
+scatter(Class1.matrix(:, 1), Class1.matrix(:, 2), 'r', 'filled'); 
+scatter(Class2.matrix(:, 1), Class2.matrix(:, 2), 'g', 'filled'); 
+hold off;
+
+figure(2)
+hold on;
+scatter(C1.matrix(:, 1), C1.matrix(:, 2), 'r', 'filled'); 
+scatter(C2.matrix(:, 1), C2.matrix(:, 2), 'g', 'filled');
+hold off;
+
 % figure(3)
 % hold on
 % plot(1:analysisError.m, analysisError.matrix(1, :));
@@ -62,7 +62,7 @@ close all;
 % title('LOO');
 % xlabel('h');
 % ylabel('Error');
-% toc
+toc
 
 % LAB 2
 % tic
@@ -109,64 +109,64 @@ close all;
 % toc
 
 % LAB 3
-tic
-k = randi([5 50]) / 10;
-b = 1;
 
-[X,Y] = meshgrid(1:100, 1:100);
-XY = [X(:) Y(:)];
-
-C1 = NMatrix(XY(find(k*X(:) + b > Y(:)), :));
-C2 = NMatrix(XY(find(k*X(:) + b < Y(:)), :));
-clear k; clear b;
-clear X; clear Y;
-
-train_set1 = NMatrix(C1.matrix(randi(C1.n, 500, 1), :));
-train_set2 = NMatrix(C2.matrix(randi(C2.n, 500, 1), :));
-
-train_set1.unique();
-train_set2.unique();
-
-C1 = train_set1.unique_matrix(C1.matrix);
-C2 = train_set2.unique_matrix(C2.matrix);
-
-control_set1 = NMatrix(C1.matrix(randi(C1.n, 170, 1), :));
-control_set2 = NMatrix(C2.matrix(randi(C2.n, 170, 1), :));
-
-control_set1.unique();
-control_set2.unique();
-
-train_set = [train_set1.matrix ones(train_set1.n, 1); 
-             train_set2.matrix zeros(train_set2.n, 1)];
-NMatrix.sattolo(train_set);
-         
-control_set = [control_set1.matrix ones(control_set1.n, 1); 
-             control_set2.matrix zeros(control_set2.n, 1)];
-
-globalError = NeuralNetwork.search_optimalNN(train_set, control_set, 5e-2);
-nn_setting = globalError{find([globalError{:, 1}] == min([globalError{:, 1}])), 2};
-
-nn = NeuralNetwork(nn_setting);
-
-nn.initW();
-[time, iteration_count] = nn.train(train_set, 1e-2, 'backpropagation');
-disp('backpropagation'); time, mean(iteration_count)
-
-nn.initW();
-[time, iteration_count] = nn.train(train_set, 1e-2, 'DBD');
-disp('DBD'); time, mean(iteration_count)
-
-figure(1)
-hold on
-scatter(control_set1.matrix(:, 1), control_set1.matrix(:, 2), 'r', 'filled');
-scatter(control_set2.matrix(:, 1), control_set2.matrix(:, 2), 'g', 'filled');
-hold off
-
-[T1, T2] = nn.classification(XY);
-
-figure(2)
-hold on
-scatter(T1.matrix(:, 1), T1.matrix(:, 2), 'r', 'filled');
-scatter(T2.matrix(:, 1), T2.matrix(:, 2), 'g', 'filled');
-hold off
-toc
+% k = randi([5 50]) / 10;
+% b = 1;
+% 
+% [X,Y] = meshgrid(1:100, 1:100);
+% XY = [X(:) Y(:)];
+% 
+% C1 = NMatrix(XY(find(k*X(:) + b > Y(:)), :));
+% C2 = NMatrix(XY(find(k*X(:) + b < Y(:)), :));
+% clear k; clear b;
+% clear X; clear Y;
+% 
+% train_set1 = NMatrix(C1.matrix(randi(C1.n, 500, 1), :));
+% train_set2 = NMatrix(C2.matrix(randi(C2.n, 500, 1), :));
+% 
+% train_set1.unique();
+% train_set2.unique();
+% 
+% C1 = train_set1.unique_matrix(C1.matrix);
+% C2 = train_set2.unique_matrix(C2.matrix);
+% 
+% control_set1 = NMatrix(C1.matrix(randi(C1.n, 170, 1), :));
+% control_set2 = NMatrix(C2.matrix(randi(C2.n, 170, 1), :));
+% 
+% control_set1.unique();
+% control_set2.unique();
+% 
+% train_set = [train_set1.matrix ones(train_set1.n, 1); 
+%              train_set2.matrix zeros(train_set2.n, 1)];
+% NMatrix.sattolo(train_set);
+%          
+% control_set = [control_set1.matrix ones(control_set1.n, 1); 
+%              control_set2.matrix zeros(control_set2.n, 1)];
+% 
+% globalError = NeuralNetwork.search_optimalNN(train_set, control_set, 5e-2);
+% nn_setting = globalError{find([globalError{:, 1}] == min([globalError{:, 1}])), 2};
+% 
+% nn = NeuralNetwork(nn_setting);
+% 
+% nn.initW();
+% [time, iteration_count] = nn.train(train_set, 1e-2, 'backpropagation');
+% disp('backpropagation'); time, mean(iteration_count)
+% 
+% nn.initW();
+% [time, iteration_count] = nn.train(train_set, 1e-2, 'DBD');
+% disp('DBD'); time, mean(iteration_count)
+% 
+% figure(1)
+% hold on
+% scatter(control_set1.matrix(:, 1), control_set1.matrix(:, 2), 'r', 'filled');
+% scatter(control_set2.matrix(:, 1), control_set2.matrix(:, 2), 'g', 'filled');
+% hold off
+% 
+% [T1, T2] = nn.classification(XY);
+% 
+% figure(2)
+% hold on
+% scatter(T1.matrix(:, 1), T1.matrix(:, 2), 'r', 'filled');
+% scatter(T2.matrix(:, 1), T2.matrix(:, 2), 'g', 'filled');
+% hold off
+% toc
